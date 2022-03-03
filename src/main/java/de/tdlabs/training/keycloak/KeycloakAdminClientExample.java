d2471426c73d308becb9f1e472cfd7118ccc22c4
a782327 <cesar.chavezmaldonado@atos.net>
2022-03-03 16:13:39 +0100

    Update.

--- src/main/java/de/tdlabs/training/keycloak/KeycloakAdminClientExample.java
+++ src/main/java/de/tdlabs/training/keycloak/KeycloakAdminClientExample.java
@@ -1,43 +1,67 @@
 package de.tdlabs.training.keycloak;
 
-import static java.util.Arrays.asList;
-
-import javax.ws.rs.core.Response;
-
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
+
+import javax.ws.rs.core.Response;
+import java.util.Collections;
 
 public class KeycloakAdminClientExample {
 
 	public static void main(String[] args) throws Exception {
+//		String realm = "master";
+		String realm = "jhipster";
+		String user = "admin";
+//		String user = "user";
+		String password = "admin";
+//		String clientId = "admin-cli";
+//		String clientId = "jakarta-school";
+		String clientId = "internal";
+//		String clientSecret = "Z1sgpbPnAuAeZWdBCQJABT9ASGkJoN1q";
+//		String clientSecret = "U8QA7NVDNJNK5vR2Z8RqRZFzi6y4E5Ki";
+		String clientSecret = "internal";
 
-		Keycloak kc = KeycloakBuilder.builder() //
-				.serverUrl("http://localhost:9080/auth") //
-				.realm("jhipster")//
-				.username("admin") //
-				.password("admin") //
-				.clientId("admin-cli") //
-				.resteasyClient(new ResteasyClientBuilder().connectionPoolSize(10).build()) //
+		Keycloak kc = KeycloakBuilder.builder()
+				.serverUrl("http://localhost:9080/auth")
+				.realm(realm)
+				.username(user)
+				.password(password)
+				.clientId(clientId)
+				.clientSecret(clientSecret)
+				.resteasyClient(new ResteasyClientBuilder().connectionPoolSize(10).build())
 				.build();
+		System.out.println(kc.tokenManager().getAccessToken().getToken());
 
 		CredentialRepresentation credential = new CredentialRepresentation();
 		credential.setType(CredentialRepresentation.PASSWORD);
 		credential.setValue("test123");
 		credential.setTemporary(false);
 
-		UserRepresentation user = new UserRepresentation();
-		user.setUsername("testuser");
-		user.setFirstName("Test");
-		user.setLastName("User");
-		user.setCredentials(asList(credential));
-		user.setEnabled(true);
-		user.setRealmRoles(asList("admin"));
+		UserRepresentation newUser = new UserRepresentation();
+		newUser.setUsername("testuser");
+		newUser.setFirstName("Test");
+		newUser.setLastName("User");
+		newUser.setCredentials(Collections.singletonList(credential));
+		newUser.setEnabled(true);
+		newUser.setRealmRoles(Collections.singletonList("admin"));
+
+		RealmResource realmResource = kc.realm(realm);
+		UsersResource userResource = realmResource.users();
+
+//		UserRepresentation userRepresentation = kc.realm(realm).users().get("admin").toRepresentation();
+		UserRepresentation userRepresentation = userResource.list().stream().filter(u -> u.getUsername().equals("user")).findFirst().orElse(null);
+		userRepresentation.setEnabled(false);
+//		userRepresentation.setEnabled(true);
+		System.out.println(userRepresentation.isEnabled());
+		userResource.get(userRepresentation.getId()).update(userRepresentation);
 
 		// Create testuser
-		Response result = kc.realm("jhipster").users().create(user);
+		Response result = kc.realm(realm).users().create(newUser);
 		if (result.getStatus() != 201) {
 			System.err.println("Couldn't create user.");
 			System.exit(0);
@@ -50,7 +74,7 @@ public class KeycloakAdminClientExample {
 		// Delete testuser
 		String locationHeader = result.getHeaderString("Location");
 		String userId = locationHeader.replaceAll(".*/(.*)$", "$1");
-		kc.realm("jhipster").users().get(userId).remove();
+		kc.realm(realm).users().get(userId).remove();
 	}
 
 }
--- src/main/java/de/tdlabs/training/keycloak/KeycloakAdminClientExampleKotlin.kt
+++ src/main/java/de/tdlabs/training/keycloak/KeycloakAdminClientExampleKotlin.kt
@@ -1,57 +1,71 @@
 package de.tdlabs.training.keycloak
 
-import java.util.Arrays.asList
-
-import javax.ws.rs.core.Response
-
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder
-import org.keycloak.admin.client.Keycloak
 import org.keycloak.admin.client.KeycloakBuilder
 import org.keycloak.representations.idm.CredentialRepresentation
 import org.keycloak.representations.idm.UserRepresentation
+import java.util.function.Predicate
 
 object KeycloakAdminClientExampleKotlin {
-
     @Throws(Exception::class)
+    @JvmStatic
     fun main(args: Array<String>) {
-
-        val kc = KeycloakBuilder.builder() //
-                .serverUrl("http://localhost:8081/auth") //
-                .realm("rest-example")//
-                .username("rest-user-admin") //
-                .password("password") //
-                .clientId("admin-cli") //
-                .resteasyClient(ResteasyClientBuilder().connectionPoolSize(10).build()) //
+//		String realm = "master";
+        val realm = "jhipster"
+        val user = "admin"
+        //		String user = "user";
+        val password = "admin"
+        //		String clientId = "admin-cli";
+//		String clientId = "jakarta-school";
+        val clientId = "internal"
+        //		String clientSecret = "Z1sgpbPnAuAeZWdBCQJABT9ASGkJoN1q";
+//		String clientSecret = "U8QA7NVDNJNK5vR2Z8RqRZFzi6y4E5Ki";
+        val clientSecret = "internal"
+        val kc = KeycloakBuilder.builder()
+                .serverUrl("http://localhost:9080/auth")
+                .realm(realm)
+                .username(user)
+                .password(password)
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .resteasyClient(ResteasyClientBuilder().connectionPoolSize(10).build())
                 .build()
-
+        println(kc.tokenManager().accessToken.token)
         val credential = CredentialRepresentation()
-        credential.setType(CredentialRepresentation.PASSWORD)
-        credential.setValue("test123")
-        credential.setTemporary(false)
+        credential.type = CredentialRepresentation.PASSWORD
+        credential.value = "test123"
+        credential.isTemporary = false
+        val newUser = UserRepresentation()
+        newUser.username = "testuser"
+        newUser.firstName = "Test"
+        newUser.lastName = "User"
+        newUser.credentials = listOf(credential)
+        newUser.isEnabled = true
+        newUser.realmRoles = listOf("admin")
+        val realmResource = kc.realm(realm)
+        val userResource = realmResource.users()
 
-        val user = UserRepresentation()
-        user.setUsername("testuser")
-        user.setFirstName("Test")
-        user.setLastName("User")
-        user.setCredentials(asList(credential))
-        user.setEnabled(true)
-        user.setRealmRoles(asList("admin"))
+//		UserRepresentation userRepresentation = kc.realm(realm).users().get("admin").toRepresentation();
+        val userRepresentation: UserRepresentation = userResource.list().first { representation: UserRepresentation? ->  representation!!.username == "user" }
+//        val userRepresentation: UserRepresentation = userResource.list(). .filter(Predicate { u: UserRepresentation -> u.username == "user" }).findFirst().orElse(null)
+        userRepresentation.isEnabled = false
+        //		userRepresentation.setEnabled(true);
+        println(userRepresentation.isEnabled)
+        userResource[userRepresentation.id].update(userRepresentation)
 
         // Create testuser
-        val result = kc.realm("rest-example").users().create(user)
-        if (result.getStatus() !== 201) {
+        val result = kc.realm(realm).users().create(newUser)
+        if (result.status != 201) {
             System.err.println("Couldn't create user.")
             System.exit(0)
         }
-        System.out.println("Testuser created.... verify in keycloak!")
-
-        System.out.println("Press any key...")
+        println("Testuser created.... verify in keycloak!")
+        println("Press any key...")
         System.`in`.read()
 
         // Delete testuser
         val locationHeader = result.getHeaderString("Location")
-        val userId = locationHeader.replace(".*/(.*)$", "$1")
-        kc.realm("rest-example").users().get(userId).remove()
+        val userId = locationHeader.replace(".*/(.*)$".toRegex(), "$1")
+        kc.realm(realm).users()[userId].remove()
     }
-
 }
